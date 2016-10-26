475f6b9446cffadb2eec8f28a17f9ef9be267c66
wenzhao107 <wzhao@outlook.com>
2016-10-26 11:53:29 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,21 +1,23 @@
-package { 'memcached':
-  ensure => present,
-}
-file { '/etc/memcached.conf':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  PORT="11211"
-  USER="memcached"
-  MAXCONN="96"
-  CACHESIZE="32"
-  OPTIONS=""
-  source => 'puppet:///etc/sysconfig/memcached/memcached.conf',
-  require => Package['memcached'],
-}
-service { 'memcached':
-  ensure => running,
-  enable => true,
-  subscribe => File['/etc/memcached.conf'],
+# memcached/manifests/init.pp
+class memcached {
+
+  package { 'memcached':
+    ensure => present,
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///etc/sysconfig/memcached/memcached.conf',
+    require => Package['memcached'],
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+
 }
