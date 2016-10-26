f2142fc88aba69d290ffb2f99e8f34e310cf2275
wenzhao107 <wzhao@outlook.com>
2016-10-26 12:12:32 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,23 +1,23 @@
 # memcached/manifests/init.pp
 class memcached {
-
   package { 'memcached':
     ensure => present,
   }
   
   file { '/etc/sysconfig/memcached':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///etc/sysconfig/memcached/memcached',
+    ensure  => file,
+    group   => 'root',
+    owner   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached',
     require => Package['memcached'],
   }
-
+  
   service { 'memcached':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    enable    => true,
     subscribe => File['/etc/sysconfig/memcached'],
   }
-
-}
+ }
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. 
