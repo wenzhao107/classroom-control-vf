090f3ddc55bc5d7872e51addae86e93c40d473d4
wenzhao107 <wzhao@outlook.com>
2016-10-26 13:21:44 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,26 @@
-# here
+# nginx/manifests/init.pp
+class nginx {
+
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www': 
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
