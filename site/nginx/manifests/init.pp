46c8ce46564e1d48cd05be4add2308acecdec8c0
wenzhao107 <wzhao@outlook.com>
2016-10-26 13:34:18 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -17,8 +17,29 @@ class nginx {
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
+    source => 'puppet:///modules/nginx/index.html',
   }
   
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
+    
   service { 'nginx':
     ensure => running,
     enable => true,
